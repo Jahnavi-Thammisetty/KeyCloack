06f0a89342d46603551eaaf34141e05298d1bd81
janu <janu.t2001@gmail.com>
2024-02-23 10:30:39 +0530

    Keycloak using OAuth2.0

---
+++ src/main/java/com/ty/controller/EmployeeController.java
@@ -0,0 +1,14 @@
+package com.ty.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class EmployeeController {
+    @GetMapping("/home")
+    public String getHome()
+    {
+        return "home";
+    }
+}
